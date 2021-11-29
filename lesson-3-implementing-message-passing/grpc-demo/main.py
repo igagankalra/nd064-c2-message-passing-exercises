37606b2e7285b2ce5afc789f87f71af9cf1268a8
Gagan Kalra <gagandeep.singh@logrhythm.com>
2021-11-29 18:06:57 +0530

    adding code

--- lesson-3-implementing-message-passing/grpc-demo/main.py
+++ lesson-3-implementing-message-passing/grpc-demo/main.py
@@ -4,6 +4,8 @@ from concurrent import futures
 import grpc
 import item_pb2
 import item_pb2_grpc
+import order_pb2
+import order_pb2_grpc
 
 
 class ItemServicer(item_pb2_grpc.ItemServiceServicer):
@@ -20,9 +22,45 @@ class ItemServicer(item_pb2_grpc.ItemServiceServicer):
         return item_pb2.ItemMessage(**request_value)
 
 
+class OrderServicer(order_pb2_grpc.OrderServiceServicer):
+    def Get(self, request, context):
+        first_order = order_pb2.OrderMessage(
+            id="2222",
+            created_by="USER123",
+            status=order_pb2.OrderMessage.Status.QUEUED,
+            created_at='2020-03-12',
+            equipment=[order_pb2.OrderMessage.Equipment.KEYBOARD]
+        )
+
+        second_order = order_pb2.OrderMessage(
+            id="3333",
+            created_by="USER123",
+            status=order_pb2.OrderMessage.Status.QUEUED,
+            created_at='2020-03-11',
+            equipment=[order_pb2.OrderMessage.Equipment.MOUSE]
+        )
+
+        result = order_pb2.OrderMessageList()
+        result.orders.extend([first_order, second_order])
+        return result
+
+    def Create(self, request, context):
+        print("Received a message!")
+
+        request_value = {
+            "id": request.id,
+            "created_by": request.created_by,
+            "status": request.status,
+            "created_at": request.created_at,
+            "equipment": ["KEYBOARD"]
+        }
+        print(request_value)
+
+        return order_pb2.OrderMessage(**request_value)
+
 # Initialize gRPC server
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
-item_pb2_grpc.add_ItemServiceServicer_to_server(ItemServicer(), server)
+order_pb2_grpc.add_OrderServiceServicer_to_server(OrderServicer(), server)
 
 
 print("Server starting on port 5005...")
--- lesson-3-implementing-message-passing/grpc-demo/writer.py
+++ lesson-3-implementing-message-passing/grpc-demo/writer.py
@@ -1,6 +1,6 @@
 import grpc
-import item_pb2
-import item_pb2_grpc
+import order_pb2
+import order_pb2_grpc
 
 """
 Sample implementation of a writer that can be used to write messages to gRPC.
@@ -9,15 +9,24 @@ Sample implementation of a writer that can be used to write messages to gRPC.
 print("Sending sample payload...")
 
 channel = grpc.insecure_channel("localhost:5005")
-stub = item_pb2_grpc.ItemServiceStub(channel)
+stub = order_pb2_grpc.OrderServiceStub(channel)
 
 # Update this with desired payload
-item = item_pb2.ItemMessage(
-    name="Non-Stick Frying Pan",
-    brand_name=10,
-    id=4,
-    weight=4.5
+# item = item_pb2.ItemMessage(
+#     name="Non-Stick Frying Pan",
+#     brand_name="chef dreams",
+#     id=4,
+#     weight=4.5
+# )
+# Update this with desired payload
+order = order_pb2.OrderMessage(
+    id="2222",
+    created_by="USER123",
+    status=order_pb2.OrderMessage.Status.QUEUED,
+    created_at='2020-03-12',
+    equipment=[order_pb2.OrderMessage.Equipment.KEYBOARD]
 )
 
 
-response = stub.Create(item)
+response = stub.Get(order)
+print(response)
