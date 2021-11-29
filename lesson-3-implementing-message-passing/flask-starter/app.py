cd05a7e525944f88bf2ae60e5d313c5e58dba8de
Gagan Kalra <gagandeep.singh@logrhythm.com>
2021-11-29 22:49:38 +0530

    more code

--- lesson-3-implementing-message-passing/flask-starter/app.py
+++ lesson-3-implementing-message-passing/flask-starter/app.py
@@ -10,5 +10,14 @@ app = Flask(__name__)
 def health():
     return jsonify({'response': 'Hello World!'})
 
+@app.route('/api/orders/computers', methods=['GET', 'POST'])
+def computers():
+    if request.method == 'GET':
+        return jsonify(retrieve_orders())
+    elif request.method == 'POST':
+        return jsonify(create_order(body=request.json))
+    else:
+        raise Exception('Unsupported HTTP request type.')
+
 if __name__ == '__main__':
     app.run()
